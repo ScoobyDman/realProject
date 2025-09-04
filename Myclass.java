919342898eafd54ab48af2811c2c497777c90391
ScoobyDman <dchrism8036@gmail.com>
2025-09-04 14:13:22 -0500

    Add files via upload

---
+++ Myclass.java
@@ -0,0 +1,30 @@
+package realproject;
+
+public class Myclass {
+
+	public static void main(String[] args) {
+		System.out.print("Don't let anyone look down on you because you are young, but set an example to believers in speech, conduct, love, faith, and in purity.\n");
+		
+		System.out.print("1 Timothy 4:12\n".toUpperCase());
+		
+		System.out.format("%30s%s", "Hi! ", "I'm Drew.\n");
+		
+		System.out.println("\twhat up my bigger");
+		
+		String g,k;
+		g = "Golden";
+		k = "Knight!";
+		
+		System.out.println(g + k);
+		
+		System.out.println("what thats not right...");
+		
+		System.out.println(g.toUpperCase() + " " + k.toUpperCase());
+		
+		
+		
+	
+
+	}
+
+}
